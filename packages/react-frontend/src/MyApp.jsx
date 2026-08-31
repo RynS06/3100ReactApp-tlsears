1060bd4dafcc18fd34350931e24a17702fc13721
RynS06 <tlsears2006@gmail.com>
2026-08-30 19:05:54 -0700

    part 9 IE1

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
+
 
 function MyApp() {
   const [characters, setCharacters] = useState([
@@ -29,10 +31,10 @@ function MyApp() {
   }
 
   return (
-    <div className="container">
-      <Table
-        characterData={characters} removeCharacter={removeOneCharacter}/>
-    </div>
+  <div className="container">
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    <Form />
+  </div>
   );
 }
 
